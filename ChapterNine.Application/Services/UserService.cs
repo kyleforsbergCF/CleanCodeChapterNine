00de816d705fbf6102c54af3a7eef888ba259bb2
Kyle Forsberg <kyleforsberg@cloudfitsoftware.com>
2025-11-06 06:08:28 -0500

    Implement UserService

--- ChapterNine.Application/Services/UserService.cs
+++ ChapterNine.Application/Services/UserService.cs
@@ -1,3 +1,4 @@
+using ChapterNine.Application.Exceptions;
 using ChapterNine.Application.Interfaces;
 using ChapterNine.Application.Models;
 
@@ -7,21 +8,35 @@ public class UserService(IUserRepository userRepository, ICacheService cacheServ
 {
     public async Task<IEnumerable<User>> GetUsersAsync(CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        var users = await cacheService.GetValueAsync<IEnumerable<User>>("users", cancellationToken);
+        if (users == null || !users.Any())
+        {
+            users = await userRepository.GetAllAsync(cancellationToken);
+            await cacheService.SetValueAsync("users", users, cancellationToken);
+        }
+        return users ?? [];
     }
 
     public async Task<User> CreateUserAsync(User user, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        if (!userValidator.IsValid(user))
+        {
+            throw new InvalidUserException();
+        }
+        return await userRepository.CreateUserAsync(user, cancellationToken);
     }
 
     public async Task<User> UpdateUserAsync(User user, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        if (!userValidator.IsValid(user))
+        {
+            throw new InvalidUserException();
+        }
+        return await userRepository.UpdateUserAsync(user, cancellationToken);
     }
 
     public async Task DeleteUserAsync(Guid id, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        await userRepository.DeleteUserAsync(id, cancellationToken);
     }
 }
